119e75e401310ecfe6393ddb8a103041c12c0491
Stan Sitnitski <stanley@Stans-MBP.socal.rr.com>
2022-09-18 22:06:03 -0700

    added lib and route folders and functionning server.js

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require("express").Router()
+const {notes} = require("../db/db.json")
+const {v4:uuidv4} = require("uuid");
+const {noteCreator} = require("../lib/lib");
+
+router.get('/notes', (req, res) => {
+  res.json(notes)
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = uuidv4()
+  let newNote = noteCreator(req.body, notes) 
+  res.json(newNote)
+});
+
+module.exports=router
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const api = require("./routes/api");
+const html = require("./routes/html");
+const app = express();
+const fs = require("fs");
+const PORT = process.env.PORT || 3001
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+
+app.use("/api", api);
+app.use("/", html);
+
+
+
+
+app.listen(PORT, () => {console.log("Server is running now.")});
